1df1baa4bd1c3a966197419d7ed04f7a081648af
Kristan Frost <kristan.frost@gmail.com>
2019-12-02 14:30:04 -0600

    working on new post submission

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import re
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:test@localhost:8889/build=a=blog'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:test@localhost:8889/build-a-blog'
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
 
@@ -25,13 +25,15 @@ class Blog(db.Model):
 def index():
 
     if request.method == 'POST':
-        blog_name = request.form['title']
-        new_blog = Blog(blog_name)
-        db.session.add(new_task)
+        blog_title = request.form['title']
+        new_blog = Blog(blog_title)
+        db.session.add(new_blog)
         db.session.commit()
 
-    # blogs = Blog.query.all()
     return render_template('newpost.html')
+
+
+ # blogs = Blog.query.all()
 
 # @app.route('/delete-task', methods=['POST'])
 # def delete_task():
